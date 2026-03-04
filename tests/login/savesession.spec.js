8f24137d69f57a61b47bcc1a6bd1aefdd92d50d4
M usman ghani <musmanghani211@gmail.com>
2026-03-04 13:09:07 +0500

    Add social media & sorting tests

---
+++ tests/login/savesession.spec.js
@@ -0,0 +1,8 @@
+// File: tests/login/saveSession.spec.js
+import { test } from '@playwright/test';
+import { login } from './possitive'; // ✅ Import your login function
+
+test('Save login session state', async ({ page }) => {
+  await login(page);
+  console.log('✅ Session saved to auth/userSession.json');
+});
